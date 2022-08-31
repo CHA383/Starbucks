a140b3b4bce271c77d5879286807bc468ce93985
chasiheon <shcha1678@naver.com>
2022-08-31 23:19:08 +0900

    init

---
+++ JS/DOMAPI 1-3.js
@@ -0,0 +1,7 @@
+// <-- DOMAPI 3 -->
+// const boxEl = document.querySelector('.box');
+
+// console.log(boxEl.textContent);
+
+// boxEl.textContent = 'Hello World';
+// console.log(boxEl.textContent);
---
+++ JS/MethodChaning.js
@@ -0,0 +1,9 @@
+// Method Chaning
+const a = 'Hello World~';
+// split: 문자를 인수 기준으로 쪼개서 배열로 반환.
+// reverse: 배열을 뒤집기.
+// join: 배열을 인수 기준으로 문자로 병합해 반환.
+
+const b = a.split('').reverse().join(''); // Method Chaning
+console.log(a); // Hello~
+console.log(b); // ~olleH
