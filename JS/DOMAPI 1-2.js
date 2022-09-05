9189777bde99ddadeb6a021741e93c4245520130
ChaSiHeon_ <67471804+CHA383@users.noreply.github.com>
2022-09-05 23:20:00 +0900

    Delete DOMAPI 1-2.js

--- JS/DOMAPI 1-2.js
+++
@@ -1,7 +0,0 @@
-// <-- DOMAPI 2 -->
-// const boxEls = document.querySelectorAll('.box');
-
-// boxEls.forEach(function (boxEl, index) {
-//     boxEl.classList.add(`order-${index + 1}`);
-//     console.log(index, boxEl);
-// })
