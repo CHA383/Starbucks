d09c63e6477d0b28d4625a9f5bb22c26e4559c18
CHA383 <shcha1678@naver.com>
2022-09-06 20:04:43 +0900

    오브젝트 플로팅 처리

--- JS/Starbucks.js
+++ JS/Starbucks.js
@@ -101,5 +101,8 @@ floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', 0.5, 15);
 floatingObject('.floating3', 1.5, 20);
 
+/* 요소가 화면에 보여짐 여부에 따른 요소 관리 */
+const spyEls = document.querySelectorAll('section.scroll-spy');
+// 요소들 반복 처리!
 
 
